590c22f90005001973122f6d3a9e9e1ffed2b8e7
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-05-20 23:28:40 +0200

    test api using axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getStoryIds } from './services/Api';
 
-function App() {
+export const App = () => {
+	useState(async () => {
+		const stories = await getStoryIds();
+		console.log('this is stories', stories);
+	}, []);
 	return <div className="App">this is app</div>;
-}
+};
 
 export default App;
