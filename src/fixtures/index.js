86c600108b9d6a773d648c951789fe2a0364fb6b
Paweł Waligóra <pawelwaligora1337@gmail.com>
2020-06-03 22:22:19 +0200

    add mock data for testing

---
+++ src/fixtures/index.js
@@ -0,0 +1,17 @@
+export const singleStory = {
+	by: 'Adam McAdams',
+	id: 1234,
+	time: Date.now(),
+	title: 'This is story',
+	url: 'https://website.com/story'
+};
+
+export const storyIds = [ 1234 ];
+
+export const emptySingleStory = {
+	by: undefined,
+	id: undefined,
+	time: undefined,
+	title: undefined,
+	url: undefined
+};
